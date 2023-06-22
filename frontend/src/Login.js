d3d018cd3ac42c2905ee54f88bf0cf9163493e2f
Gazi-Nasim <gazinasim001@gmail.com>
2023-06-22 11:37:49 +0600

    git login form modify

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,39 +1,39 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-     const [mail, setmail] = useState('');
-     const [pass, setpass] = useState('');
-     const [error, seterror] = useState('Sign in to start your session');
-     const navigate=useNavigate();
-     const login=()=>{
+    const [mail, setmail] = useState('');
+    const [pass, setpass] = useState('');
+    const [error, seterror] = useState('Sign in to start your session');
+    const navigate = useNavigate();
+    const login = () => {
         axios.post('http://fleet.prantiksoft.com/backend/Login', {
-        email: mail,
-        password: pass
-      },{
-        headers: {
-          'Content-Type': 'application/json'
+            email: mail,
+            password: pass
+        }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            let data = response.data;
+            console.log(data);
+            if (data.status == true) {
+                localStorage.setItem('token', data.token)
+                localStorage.setItem("name", data.name)
+
+                navigate("/dashboard");
+            } else {
+                seterror('Something went wrong!')
+            }
+        })
+    }
+    useEffect(() => {
+        let token = localStorage.getItem('token')
+        if (token != null) {
+            navigate("/dashboard");
         }
-      }).then(function (response) {
-        let data=response.data;
-        console.log(data);
-        if(data.status==true){
-          localStorage.setItem('token',data.token)
-          localStorage.setItem("name",data.name)
-          
-          navigate("/dashboard");
-        }else{
-          seterror('Something went wrong!')
-        }
-      })
-     }
-     useEffect(() => {
-        let token=localStorage.getItem('token')
-        if(token!=null){
-          navigate("/dashboard");
-        }
-      }, []);
+    }, []);
     return (
         <>
             <div className='hold-transition login-page'>
@@ -44,32 +44,35 @@ const Login = () => {
                     <div className="card">
                         <div className="card-body login-card-body">
                             <p className="login-box-msg">{error}</p>
-                                <div className="input-group mb-3">
-                                    <input type="email" className="form-control" placeholder="Email" onChange={(e)=>setmail(e.target.value)}/>
-                                    <div className="input-group-append">
-                                        <div className="input-group-text">
-                                            <span className="fas fa-envelope"></span>
-                                        </div>
+                            <div className="input-group mb-3">
+                                <input type="email" className="form-control" placeholder="Email" onChange={(e) => setmail(e.target.value)} />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-envelope"></span>
                                     </div>
                                 </div>
-                                <div className="input-group mb-3">
-                                    <input type="password" className="form-control" placeholder="Password" onChange={(e)=>setpass(e.target.value)}/>
-                                    <div className="input-group-append">
-                                        <div className="input-group-text">
-                                            <span className="fas fa-lock"></span>
-                                        </div>
+                            </div>
+                            <div className="input-group mb-3">
+                                <input type="password" className="form-control" placeholder="Password" onChange={(e) => setpass(e.target.value)} />
+                                <div className="input-group-append">
+                                    <div className="input-group-text">
+                                        <span className="fas fa-lock"></span>
                                     </div>
                                 </div>
-                                <div className="row">
-                                    <div className="col-8">
-                                        <div className="icheck-primary">
+                            </div>
+                            <div className="row">
+                                <div className="col-8">
+                                    <div className="icheck-primary">
 
-                                        </div>
-                                    </div>
-                                    <div className="col-4">
-                                        <button type="submit" className="btn btn-primary btn-block" onClick={login}>Sign In</button>
                                     </div>
                                 </div>
+                                <div className="col-4">
+                                    <button type="submit" className="btn btn-primary btn-block" onClick={login}>Sign In</button>
+                                </div>
+                                <h5>To login use admin@email.com / user@email.com / drier@email.com</h5>
+                                <h5>Password To Login 12345 </h5>
+
+                            </div>
                         </div>
                     </div>
                 </div>
